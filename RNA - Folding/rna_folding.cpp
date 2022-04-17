f1f9139fb70b89f186493a228c4e2ff8fe9f63d1
R. Adarsh <66625110+Adarsh2801-r@users.noreply.github.com>
2022-04-17 18:04:30 +0530

    Update rna_folding.cpp

--- RNA - Folding/rna_folding.cpp
+++ RNA - Folding/rna_folding.cpp
@@ -1,9 +1,37 @@
 #include<bits/stdc++.h>
+#include <fstream>
+
 
 using namespace std;
 
+vector<pair<int,int>>base_pairs;
+
 int match(char c1,char c2){
 	return (c1=='A'&&c2=='U')||(c1=='U'&&c2=='A')||(c1=='C'&&c2=='G')||(c1=='G'&&c2=='C');
+}
+
+void retrieve_structure(string input_seq,vector<vector<int>>&dp,int x,int y,int n){
+	if(x > n || y < 1){
+		return;
+	}
+	if(dp[x][y] == dp[x][y-1]){
+		retrieve_structure(input_seq,dp,x,y-1,n);
+	}
+	else{
+		for(int t = x; t<y-4;t++){
+
+				if(1+dp[x][t-1]+dp[t+1][y-1] == dp[x][y]){
+					if(match(input_seq[t],input_seq[y])){
+						base_pairs.push_back({t,y});
+						retrieve_structure(input_seq,dp,x,t-1,n);
+						retrieve_structure(input_seq,dp,t+1,y-1,n);
+						break;
+					}
+				}
+		}
+	}
+
+
 }
 
 
@@ -11,7 +39,6 @@ int main(){
 	string input_seq;
 	cin>>input_seq;
 	input_seq.insert(0,1,'$');
-	cout<<input_seq<<endl;
 	int n = input_seq.size();
 	vector<vector<int>> dp(n+1,vector<int>(n+1,0));
 	for (int i = 0; i <= n; ++i)
@@ -23,7 +50,6 @@ int main(){
 			}
 		}
 	}
-	set<pair<int,int>>base_pairs;
 	for(int k=5;k<=n-1;k++){
 		for(int i = 1;i<=n-k;i++){
 			int j = i+k;
@@ -46,10 +72,22 @@ int main(){
 		}
 		cout<<endl;
 	}*/
+    ofstream fout; 
+    fout.open("C:/Users/BITS-PC/Desktop/DAA Assignment-1 final/RNA secondary struct prediction/RNA_secondary_structures.txt",std::ios_base::app);
+    fout<<"Input sequence : "<<input_seq<<endl;
+	cout<<"Max base pairs possible = "<<dp[1][n]<<endl;
+	fout<<"Max base pairs possible = "<<dp[1][n]<<endl;
+    cout<<"Base Pairings"<<endl;
+    fout<<"Base Pairings"<<endl;
+
+	retrieve_structure(input_seq,dp,1,n,n);
 	for(auto i:base_pairs){
-		cout<<i.first<<"===="<<i.second<<endl;
+		cout<<input_seq[i.first]<<"("<<i.first<<")"<<"===="<<input_seq[i.second]<<"("<<i.second<<")"<<endl;
+		fout<<input_seq[i.first]<<"("<<i.first<<")"<<"===="<<input_seq[i.second]<<"("<<i.second<<")"<<endl;
+
 	}
-	cout<<dp[1][n]<<endl;
+	fout<<"------------------------------------------------------------------------------------------------"<<endl;
+	fout.close();
 
 	return 0;
 }
